b0e8bd043e33cece398254f3d23a649650284b56
Luke <yuta.k.200628@gmail.com>
2023-05-04 10:54:36 +0900

    applied Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { DefaultSeo } from 'next-seo';
 import '../styles/global.css'
 
 import { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
     const [show_screen, setShowScreen] = useState(false)
@@ -13,33 +14,34 @@ export default function App({ Component, pageProps }: AppProps) {
 
     return (
         <>
-            {show_screen ? 
-            <>
-                <DefaultSeo 
-                    defaultTitle="Luke's Home"
-                    description='Student / Programmer'
-                    openGraph={{
-                        type: "wesite",
-                        title: "Luke's Home",
-                        description: "Student / Programmer",
-                        siteName: "Luke's HomePage",
-                        url: "https://luke256.dev",
-                        images: [
-                            {
-                                url: "https://luke256.dev/FlameSword.png",
-                                alt: "Flame Sword",
-                                type: "image/png"
-                            },
-                        ],
-                    }}
-                    twitter={{
-                        handle: '@luke02561',
-                        cardType: "summary"
-                    }}
-                />
-                <Component {...pageProps} />
-            </>
-             : ''}
+            {show_screen ?
+                <>
+                    <DefaultSeo
+                        defaultTitle="Luke's Home"
+                        description='Student / Programmer'
+                        openGraph={{
+                            type: "wesite",
+                            title: "Luke's Home",
+                            description: "Student / Programmer",
+                            siteName: "Luke's HomePage",
+                            url: "https://luke256.dev",
+                            images: [
+                                {
+                                    url: "https://luke256.dev/FlameSword.png",
+                                    alt: "Flame Sword",
+                                    type: "image/png"
+                                },
+                            ],
+                        }}
+                        twitter={{
+                            handle: '@luke02561',
+                            cardType: "summary"
+                        }}
+                    />
+                    <Component {...pageProps} />
+                    <Analytics />
+                </>
+                : ''}
         </>
     );
 }
