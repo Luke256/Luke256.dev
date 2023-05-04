268cead904b732542ef7bb9d1a8715fba37c38ff
Luke256 <yuta.k.200628@gmail.com>
2023-05-04 16:52:23 +0900

    setup SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 import '../styles/global.css'
 
 import { useState, useEffect } from 'react';
@@ -7,14 +8,38 @@ export default function App({ Component, pageProps }: AppProps) {
     const [show_screen, setShowScreen] = useState(false)
 
     useEffect(() => {
-      setShowScreen(true)
+        setShowScreen(true)
     }, [])
 
     return (
         <>
-          {show_screen ? <>
-            <Component {...pageProps} />
-          </> : ''}
+            {show_screen ? 
+            <>
+                <DefaultSeo 
+                    defaultTitle="Luke's Home"
+                    description='Student / Programmer'
+                    openGraph={{
+                        type: "wesite",
+                        title: "Luke's Home",
+                        description: "Student / Programmer",
+                        siteName: "Luke's HomePage",
+                        url: "https://luke256.dev",
+                        images: [
+                            {
+                                url: "https://luke256.dev/FlameSword.png",
+                                alt: "Flame Sword",
+                                type: "image/png"
+                            },
+                        ],
+                    }}
+                    twitter={{
+                        handle: '@luke02561',
+                        cardType: "summary"
+                    }}
+                />
+                <Component {...pageProps} />
+            </>
+             : ''}
         </>
     );
 }
