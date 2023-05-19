7f3a3d9ddb1607687eba0c469379a9deff618a2c
Luke <yuta.k.200628@gmail.com>
2023-05-20 03:19:37 +0900

    OGPいじった

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,33 +14,33 @@ export default function App({ Component, pageProps }: AppProps) {
 
     return (
         <>
+            <DefaultSeo
+                defaultTitle="Luke's Home"
+                description='Student / Programmer'
+                openGraph={{
+                    type: "website",
+                    title: "Luke's Home",
+                    description: "Student / Programmer",
+                    siteName: "Luke's HomePage",
+                    url: "https://luke256.dev",
+                    images: [
+                        {
+                            url: "https://luke256.dev/FlameSword.png",
+                            alt: "Flame Sword",
+                            type: "image/png"
+                        },
+                    ],
+                }}
+                twitter={{
+                    handle: '@luke02561',
+                    site: '@luke02561',
+                    cardType: "summary"
+                }}
+            />
+            <Analytics />
             {show_screen ?
                 <>
-                    <DefaultSeo
-                        defaultTitle="Luke's Home"
-                        description='Student / Programmer'
-                        openGraph={{
-                            type: "website",
-                            title: "Luke's Home",
-                            description: "Student / Programmer",
-                            siteName: "Luke's HomePage",
-                            url: "https://luke256.dev",
-                            images: [
-                                {
-                                    url: "https://luke256.dev/FlameSword.png",
-                                    alt: "Flame Sword",
-                                    type: "image/png"
-                                },
-                            ],
-                        }}
-                        twitter={{
-                            handle: '@luke02561',
-                            site: '@luke02561',
-                            cardType: "summary"
-                        }}
-                    />
                     <Component {...pageProps} />
-                    <Analytics />
                 </>
                 : ''}
         </>
