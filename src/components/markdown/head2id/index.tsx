6b474d7eb489b664a6e51f668b78909d866b9bfa
Luke256 <yuta.k.200628@gmail.com>
2023-07-15 00:31:14 +0900

    idをgithub-slugで作らせるように

--- src/components/markdown/head2id/index.tsx
+++ src/components/markdown/head2id/index.tsx
@@ -1,14 +1,17 @@
 import React from "react"
+import GithubSlugger from 'github-slugger'
 
 type Props = {
     children: any,
 }
 
+const slugger = new GithubSlugger()
 
-const Head2Id: React.FC<Props> = ({children}) => (
-    children.toString()
-        .replace(" ", "-")
-        .replace(".","")
-)
+
+const Head2Id: React.FC<Props> = ({children}) => {
+    const str: string = children.toString()
+    const id: string = slugger.slug(str)
+    return id.slice(0, -2);
+}
 
 export default Head2Id
