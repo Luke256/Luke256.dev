98439416e393ea7ef1819424af7a1921a29f0fa8
Luke256 <yuta.k.200628@gmail.com>
2023-07-15 10:07:30 +0900

    微修正

--- src/components/markdown/head2id/index.tsx
+++ src/components/markdown/head2id/index.tsx
@@ -11,7 +11,7 @@ const slugger = new GithubSlugger()
 const Head2Id = (value: string) => {
     const str: string = value
     const id: string = slugger.slug(str)
-    return id.slice(0, -2);
+    return id;
 }
 
 export default Head2Id
