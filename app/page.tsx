d572786778ea66b2237585da366cf64dd9d42e49
Luke <55425834+Luke256@users.noreply.github.com>
2025-04-08 00:21:19 +0900

    hoge

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
+import Card from "@/components/card";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex object-top items-center justify-center p-10">
-        <h1 className="font-mono font-extrabold bg-gradient-to-r from-blue-400 to-fuchsia-400 bg-clip-text text-6xl font-extrabold text-transparent">
-          Luke256&#39;s Portfolio
+      <Card>
+        <h1>
+          Hello, World!
         </h1>
-        
-        <Image src="/images/FlameSword.png" alt="Luke256" width={200} height={200} className="ml-5" />
-      </div>
+      </Card>
     </main>
   );
 }
---
+++ components/card.tsx
@@ -0,0 +1,10 @@
+
+const Card = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-4">
+      {children}
+    </div>
+  );
+}
+
+export default Card;
