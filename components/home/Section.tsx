98ae50ecea43c42d32aeba6cb459da27e693b74e
Luke <55425834+Luke256@users.noreply.github.com>
2025-04-11 00:03:38 +0900

    centered heading

--- components/home/Section.tsx
+++ components/home/Section.tsx
@@ -12,7 +12,7 @@ const Section = ({ title, children }: Props) => {
     <div className="text-white p-4 max-w-3xl mt-10 mb-10 content-center md:justify-self-center">
       <TreeSVG className="top-(--header-height) left-0 h-[calc(100%-var(--header-height))] invisible md:visible" aspectRatio={1.8} />
       <TreeSVG className="top-(--header-height) right-0 h-[calc(100%-var(--header-height))]" aspectRatio={1.8} invert />
-      <div className="text-4xl font-bold pb-2 px-8 border-b-3 border-dashed border-b-indigo-400 w-max">{title}</div>
+      <div className="text-4xl font-bold pb-2 px-8 border-b-3 border-dashed border-b-indigo-400 w-max m-auto">{title}</div>
       <div className="p-10 ">
         {children}
       </div>
