f028de3e727081d016af40ef3b5afd64e96ec508
Luke <55425834+Luke256@users.noreply.github.com>
2025-04-12 17:16:17 +0900

    add KaTeX

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
@@ -11,7 +13,11 @@ const withMDX = createMDX({
   options: {
     remarkPlugins: [
       remarkGfm,
-    ]
+      remarkMath
+    ],
+    rehypePlugins: [
+      rehypeKatex
+    ],
   }
 })
  
