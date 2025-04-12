15514ef40985f43326ddd1b5f0fba73048c2b00f
Luke <55425834+Luke256@users.noreply.github.com>
2025-04-12 21:28:30 +0900

    all blogs

---
+++ app/blogs/pck2023yo/metadata.ts
@@ -0,0 +1,9 @@
+import type { BlogInfo } from "@/components/blogs/BlogPage";
+
+const bloginfo: BlogInfo = {
+    title: "パソコン甲子園2023参加記 予選篇",
+    createdAt: "2023-09-10",
+    updatedAt: "2023-09-10",
+}
+
+export default bloginfo;
